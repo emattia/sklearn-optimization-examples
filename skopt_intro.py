fb106d344dc1e0ede4239a75527759cf4984f82c
emattia <emattia4495@gmail.com>
2021-12-15 17:24:24 -0600

    testing skopt_intro

--- skopt_intro.py
+++ skopt_intro.py
@@ -2,16 +2,30 @@ import numpy as np
 np.random.seed(123)
 import matplotlib.pyplot as plt
 
-from skopt import BayesSearchCV
+from skopt.searchcv import SigOptSearchCV, BayesSearchCV
 from sklearn.datasets import load_digits
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import make_scorer, accuracy_score, fbeta_score
+
+import sigopt
+
+# sigopt.set_project('random')
 
 X, y = load_digits(n_class=10, return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75, test_size=.25, random_state=0)
 
 # log-uniform: understand as search over p = exp(x) by varying x
-opt = BayesSearchCV(
+def f(clf, x, y):
+  return {'score': -1}
+
+my_config = {
+    'name': 'My Exp',
+    # 'metrics': [{'name': 'accuracy'}],
+    'budget': 5
+}
+
+opt = SigOptSearchCV(
     SVC(),
     {
         'C': (1e-6, 1e+6, 'log-uniform'),
@@ -20,10 +34,15 @@ opt = BayesSearchCV(
         'kernel': ['linear', 'poly', 'rbf'],  # categorical parameter
     },
     n_iter=2,
-    cv=3
+    cv=2,
+    # scoring={'f1': 'f1', 'acc': 'accuracy', 'acc2': make_scorer(accuracy_score), 'f2': make_scorer(fbeta_score, beta=2)},
+    #scoring = f, #refit='score'
+    #scoring=['accuracy', 'f1'],
+    project_id = 'random',
+    experiment_config = my_config
 )
 
 opt.fit(X_train, y_train)
 
 print("val. score: %s" % opt.best_score_)
-print("test score: %s" % opt.score(X_test, y_test))
+# print("test score: %s" % opt.score(X_test, y_test))
