7412b7adc93da3d0d190fee8ed3c8334f64a528d
emattia <emattia4495@gmail.com>
2022-01-25 17:20:02 -0600

    GBR existing run

--- sigopt-sklearn-examples/regression/runs/simplest.py
+++ sigopt-sklearn-examples/regression/runs/simplest.py
@@ -6,11 +6,13 @@ data=load_diabetes()
 X, y = data.data, data.target
 train_x,test_x,train_y,test_y = train_test_split(X,y)
 sigopt.set_project('random')
+existing_run = sigopt.create_run('My Existing Run')
 run_context = sigopt.sklearn.run(
+  GradientBoostingRegressor(), 
   train_x, 
   train_y, 
-  GradientBoostingRegressor(), 
   params={"max_depth": 4, "min_samples_split": 5,"learning_rate": 0.01},
-  validation_sets=[(test_x, test_y, "test")]
+  validation_sets=[(test_x, test_y, "test")],
+  run_options={'run': existing_run}
 )
 run_context.run.end()
