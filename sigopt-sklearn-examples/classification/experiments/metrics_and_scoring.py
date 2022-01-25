2119480e78673ddcf5415ccc696fd1d95339bcab
emattia <emattia4495@gmail.com>
2022-01-25 16:55:51 -0600

    change metric scoring exp example

--- sigopt-sklearn-examples/classification/experiments/metrics_and_scoring.py
+++ sigopt-sklearn-examples/classification/experiments/metrics_and_scoring.py
@@ -7,18 +7,16 @@ data=load_wine()
 X,y=data.data,data.target
 train_x,test_x,train_y,test_y = train_test_split(X,y)
 sigopt.set_project('random')
-experiment_config = {
-  'parameters': [{'name': 'C', 'type': 'double', 'bounds': {'min':0.1, 'max':5}}],
-  'metrics': [{'name': 'test - accuracy'}],
-   # metrics should be one of "accuracy", "F1", "precision", "recall", or a custom metric using scoring 
-  'budget': 1
-}
 experiment = sigopt.sklearn.experiment(
   SVC(), 
   train_x, 
   train_y, 
   fixed_params={'kernel': 'rbf'},
   validation_sets=[(test_x, test_y, "test")], 
-  # scoring={'my-acc': lambda estimator, X, y_true: numpy.mean(estimator.predict(X) == y_true)},
-  experiment_config=experiment_config
+  scoring={'my-acc': lambda estimator, X, y_true: numpy.mean(estimator.predict(X) == y_true)},
+  experiment_design={
+    'parameters': [{'name': 'C', 'type': 'double', 'bounds': {'min':0.1, 'max':5}}],
+    'metrics': [{'name': 'test - my-acc'}],
+    'budget': 1
+  }
 )
