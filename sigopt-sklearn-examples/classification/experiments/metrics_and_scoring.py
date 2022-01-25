3a6b3393ef5cc731ec971f7b47848a577d2eab0b
emattia <emattia4495@gmail.com>
2022-01-25 16:56:26 -0600

    change metrics and scoring metric name

--- sigopt-sklearn-examples/classification/experiments/metrics_and_scoring.py
+++ sigopt-sklearn-examples/classification/experiments/metrics_and_scoring.py
@@ -13,10 +13,10 @@ experiment = sigopt.sklearn.experiment(
   train_y, 
   fixed_params={'kernel': 'rbf'},
   validation_sets=[(test_x, test_y, "test")], 
-  scoring={'my-acc': lambda estimator, X, y_true: numpy.mean(estimator.predict(X) == y_true)},
+  scoring={'my accuracy': lambda estimator, X, y_true: numpy.mean(estimator.predict(X) == y_true)},
   experiment_design={
     'parameters': [{'name': 'C', 'type': 'double', 'bounds': {'min':0.1, 'max':5}}],
-    'metrics': [{'name': 'test - my-acc'}],
+    'metrics': [{'name': 'test - my accuracy'}],
     'budget': 1
   }
 )
